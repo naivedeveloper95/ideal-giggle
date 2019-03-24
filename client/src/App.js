6cab80cc9959a7bc8d1827e18b853af0a0987088
naivedeveloper95 <findsatyam95@gmail.com>
2019-03-25 00:52:10 +0530

    UI added. Funcationality is yet to be added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,205 +1,89 @@
 import React from 'react';
-import styles from './App.css';
+import './App.css';
 
 const App = () => {
 	return (
 		<div>
-			<div className={styles.SDKItem}>
-				<h2>Paasmer Edge V2.1</h2>
-				<p>
-					The Paasmer Edge V2.1 for SBC running any Linux flavoured
-					OS, is a collection of Docker containers that enables you to
-					do Analytics on edge and connect to the Paasmer IoT
-					Platform. The Paasmer Edge Versoin 2.1 also consists of
-					Paasmer Python library and Python SDK to connect to the
-					Paasmer Gateway. Python Library now simplifies the
-					Subscribing and publishing and is capable to handle various
-					feed types in a single function call.
-				</p>
-			</div>
-			<div className={styles.SDKItem}>
-				<h2>Paasmer-Edge-Analytics-Docker-V-2</h2>
-				<p>
-					The Paasmer-Edge-Analytics-Docker-V-2 for Raspberry-pi
-					Running Paasmer OS is a collection of Docker containers that
-					enables you to do Analytics on edge and connect to the
-					Paasmer IoT Platform. The Paasmer-Edge-Analytics-Docker-V-2
-					also consists of Paasmer Python library and Python SDK to
-					connect to the Paasmer Gateway.
-				</p>
-			</div>
-
-			<div className={styles.SDKItem}>
-				<h2>Paasmer-Python-Library-V-1</h2>
-				<p>
-					Paasmer Python Library is set of functions that can be used
-					to connect, subscribe and publish to the Paasmer gateway
-					easily and more efficiently in a single function call. It
-					can also support Edge Analytics on any feed while
-					publishing. It has separate CallBack for each feed and thus
-					making the connection simple.
-				</p>
-			</div>
-
-			<div className={styles.SDKItem}>
-				<h2>Paasmer-Edge-Docker-V-2</h2>
-				<p>
-					The Paasmer-Edge-Docker-V-2 for Raspberry-pi Running Paasmer
-					OS is a collection of Docker containers that enables you to
-					do Analytics on edge and connect to the Paasmer IoT
-					Platform. The Paasmer-Edge-Docker-V-2 also consists of
-					Paasmer Python library and Python SDK to connect to the
-					Paasmer Gateway.
-				</p>
-			</div>
-
-			<div className={styles.SDKItem}>
-				<h2>Paasmer-Edge-ML-Docker-V-1</h2>
-				<p>
-					The Paasmer-Edge-ML-Docker-V-1 for Raspberry-pi Running
-					Paasmer OS is a collection of Docker containers with a
-					sample Machine Learning Docker model that monitors the
-					Health condition based on the Blood Pressure and Blood Sugar
-					level sent from an sample Android App.
-				</p>
-			</div>
-
-			<div className={styles.SDKItem}>
-				<h2>Paasmer-CoAP-V-1</h2>
-				<p>
-					Paasmer CoAP V-1 is a collection of source files that
-					enables you to connect to the Paasmer IoT Platform. It uses
-					CoAP protocol (Constrained Application Protocol) as a
-					transport client.
-				</p>
-			</div>
-
-			<div className={styles.SDKItem}>
-				<h2>Paasmer-Edge-Docker-V-1</h2>
-				<p>
-					The Paasmer-Edge-Docker-V-1 for Raspberry-pi Running Paasmer
-					OS is a collection of Docker containers that enables you to
-					do some analytics on edge and to connect to the Paasmer IoT
-					Platform. This Paasmer-Edge-Docker-V-1 is equipped with
-					Zigbee support along with Board GPIO's.
-				</p>
-			</div>
-
-			<div className={styles.SDKItem}>
-				<h2>SBC-JAVA-SDK-V2_0_3_2</h2>
-				<p>
-					The Paasmer SDK for Single Board Computers (SBC) like
-					Raspberry-PI is a collection of source files that enables
-					you to connect to the Paasmer IoT Platform. It includes the
-					transport client for MQTT with TLS support. It is
-					distributed in source form and intended to be built into
-					customer firmware along with application code, other
-					libraries and RTOS.
-				</p>
-			</div>
-
-			<div className={styles.SDKItem}>
-				<h2>SBC-C-SDK-V2_0_1_5</h2>
-				<p>
-					The Paasmer SDK for Single Board Computers (SBC) like
-					Raspberry-PI, Banana-PI, Orange-PI, Odroidxu4, BeagleBone
-					and also for CC3200 LaunchPad XL is a collection of source
-					files that enables you to connect to the Paasmer IoT
-					Platform. It includes the transport client for MQTT with TLS
-					support. It is distributed in source form and intended to be
-					built into customer firmware along with application code,
-					other libraries and RTOS.This SDK is equipped with the
-					Zigbee, WiFi and Bluetooth (BLE) protocol support along with
-					Board GPIO's for SBC's.
-				</p>
-			</div>
-			<div className={styles.SDKItem}>
-				<h2>Paasmer-Node</h2>
-				<p>
-					The Paasmer Nodes for Node-RED are created for allowing
-					configuration, connectivity, sensor data sharing and control
-					of sensors from Node-RED or the Paasmer IoT platform. The
-					Paasmer Nodes are written in Node.JS and are available as
-					the npm repositories.
-				</p>
-			</div>
-			<div className={styles.SDKItem}>
-				<h2>SBC-Python-SDK-V_2_0_2_2</h2>
-				<p>
-					The Paasmer SBC-Python-SDK-V_2.0.2.2 for Single Board
-					Computers (SBC) like Raspberry-PI, Banana-PI, Orange-PI,
-					Odroidxu4 and BeagleBone is a collection of source files
-					that enables you to connect to the Paasmer IoT Platform. It
-					includes the transport client for MQTT with TLS support. It
-					is distributed in source form and intended to be built into
-					customer firmware along with application code, other
-					libraries and RTOS.This SDK is equipped with the Zigbee,
-					WiFi and BLE support along with Board GPIO's.
-				</p>
-			</div>
-			<div className={styles.SDKItem}>
-				<h2>SBC-C-SDK-V2_0_1_4</h2>
-				<p>
-					The Paasmer SDK for Single Board Computers (SBC) like
-					Raspberry-PI, Banana-PI, Orange-PI, Odroidxu4, BeagleBone
-					and also for CC3200 LaunchPad XL is a collection of source
-					files that enables you to connect to the Paasmer IoT
-					Platform. It includes the transport client for MQTT with TLS
-					support. It is distributed in source form and intended to be
-					built into customer firmware along with application code,
-					other libraries and RTOS.This SDK is equipped with the
-					Zigbee support along with Board GPIO's for SBC's.
-				</p>
-			</div>
-
-			<div className={styles.SDKItem}>
-				<h2>ESP-SDK-V2_0_1_3</h2>
-				<p>
-					The Paasmer SDK for ESP based Devices is a collection of
-					source files that enables you to connect to the Paasmer IoT
-					Platform. It includes the transport client for MQTT with TLS
-					support. It is distributed in source form and intended to be
-					built into customer firmware along with application code,
-					other libraries and RTOS.
-				</p>
-			</div>
-			<div className={styles.SDKItem}>
-				<h2>Arduino Yun-C-SDK-V2_0_1_2</h2>
-				<p>
-					This document provides step by step instructions to install
-					the Arduino Yún SDK and connect your device to the PAASMER
-					IoT. The PAASMER-IoT-Arduino-Yún-SDK which takes use of the
-					resources of two chips on Arduino Yún for functionality and
-					connections to the PAASMER IoT.
-				</p>
-			</div>
-			<div className={styles.SDKItem}>
-				<h2>ION-BLOC SDK-V1_2</h2>
-				<p>
-					The ION-BLOC SDK will help the developers and the users to
-					connect to our existing Private Blockchain network, through
-					which they can interact with their Raspberry Pi based on the
-					smarts contracts. This SDK will also help the developers to
-					deploy their own smart contract to control the remote
-					Raspberry Pi.
-				</p>
-				<p>
-					The IonBloc SDK version 1.2 offers automation of compilation
-					and deployment process of solidity contract on the
-					blockchain.
-				</p>
-			</div>
-			<div className={styles.SDKItem}>
-				<h2>ION-TOR-SDK</h2>
-				<p>
-					The ION-TOR SDK for Single Board Computers (SBC) like
-					Raspberry-PI, Intel Edison, Beagle Bone is a collection of
-					source files that enables you to connect to the ION-TOR
-					service. It includes the TOR libraries to connect to TOR
-					network. It is distributed in the application form and
-					intended to be built into customer solution along with other
-					libraries.
-				</p>
+			<nav class="navbar navbar-light bg-dark">
+				<a class="navbar-brand colorClass">BuyIt</a>
+				<form class="form-inline">
+					<input
+						class="form-control mr-sm-2"
+						type="search"
+						placeholder="Search"
+						aria-label="Search"
+					/>
+					<button
+						class="btn btn-outline-success my-2 my-sm-0"
+						type="submit"
+					>
+						Search
+					</button>
+				</form>
+			</nav>
+			<div class="container-fluid">
+				<div class="row content">
+					<div class="col-sm-2 styleClass">
+						<p>
+							<input type="checkbox" />
+							Type1
+						</p>
+						<p>
+							<input type="checkbox" />
+							Type2
+						</p>
+						<p>
+							<input type="checkbox" />
+							Type3
+						</p>
+					</div>
+					<div class="col-sm-10 styleClass">
+						<div class="card-deck">
+							<div class="card">
+								<div class="card-body">
+									<h5 class="card-title">Product 1</h5>
+									<p class="card-text">
+										This is a static entry. Not yet
+										integrated with db.
+									</p>
+									<p class="card-text">
+										<small class="text-muted">
+											Last updated 3 mins ago
+										</small>
+									</p>
+								</div>
+							</div>
+							<div class="card">
+								<div class="card-body">
+									<h5 class="card-title">New Products</h5>
+									<p class="card-text">
+										This is a static entry. Not yet
+										integrated with db
+									</p>
+									<p class="card-text">
+										<small class="text-muted">
+											Last updated 3 mins ago
+										</small>
+									</p>
+								</div>
+							</div>
+							<div class="card">
+								<div class="card-body">
+									<h5 class="card-title">Other product</h5>
+									<p class="card-text">
+										This is a static entry. Not yet
+										integrated with db
+									</p>
+									<p class="card-text">
+										<small class="text-muted">
+											Last updated 3 mins ago
+										</small>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
